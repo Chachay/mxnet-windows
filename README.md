6372947d3b7b1bfe3e5f54e98a8cb23cb223b561
Chachay <git@chachay.org>
2022-09-14 06:02:12 +0900

    Add more further steps on github actions

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # mxnet-windows
-Github action to build mxnet windows build 
+Github action to build mxnet windows build
+
+## For Local Problem
+```
+%CUDA_PATH%\extras\visual_studio_integration\MSBuildExtensions
+%programfiles(x86)%\Microsoft Visual Studio\2022\BuildTools
+C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Microsoft\VC\v160\BuildCustomizations
+```
+
+- [c\+\+ \- CUDA compile problems on Windows, Cmake error: No CUDA toolset found \- Stack Overflow](https://stackoverflow.com/questions/56636714/cuda-compile-problems-on-windows-cmake-error-no-cuda-toolset-found)
+- [Matching CUDA arch and CUDA gencode for various NVIDIA architectures \- Arnon Shimoni](https://arnon.dk/matching-sm-architectures-arch-and-gencode-for-various-nvidia-cards/)
--- scripts/install_cuda.ps1
+++ scripts/install_cuda.ps1
@@ -1,12 +1,11 @@
-Write-Output "Downloading CUDA toolkit 11.3. ..."
-Invoke-WebRequest https://developer.download.nvidia.com/compute/cuda/11.3.0/network_installers/cuda_11.3.0_win10_network.exe -OutFile setup.exe | Out-Null
-Write-Output "Installing CUDA toolkit 11.3 ..."
-Start-Process -Wait -FilePath .\setup.exe -ArgumentList "-s nvcc_11.3 cublas_11.3 cublas_dev_11.3 cudart_11.3 curand_11.3 curand_dev_11.3 nvml_dev_11.3"
-
-if (!$?) {
-  Write-Output "CUDA Install Error: $($LASTEXITCODE)"
-  exit 1
-}
+#Write-Output "Downloading CUDA toolkit 11.3. ..."
+#Invoke-WebRequest https://developer.download.nvidia.com/compute/cuda/11.3.0/network_installers/cuda_11.3.0_win10_network.exe -OutFile setup.exe | Out-Null
+#Write-Output "Installing CUDA toolkit 11.3 ..."
+#Start-Process -Wait -FilePath .\setup.exe -ArgumentList "-s nvcc_11.3 cublas_11.3 cublas_dev_11.3 cudart_11.3 curand_11.3 curand_dev_11.3 nvml_dev_11.3 visual_studio_integration_11.3"
+#if (!$?) {
+#  Write-Output "CUDA Install Error: $($LASTEXITCODE)"
+#  exit 1
+#}
 
 Write-Output "Downloading cudnn"
 Invoke-WebRequest https://developer.download.nvidia.com/compute/redist/cudnn/v8.4.1/local_installers/11.6/cudnn-windows-x86_64-8.4.1.50_cuda11.6-archive.zip -OutFile cudnn.zip | Out-Null
@@ -23,9 +22,8 @@ Copy-Item -Path "d:\a\cudnn\cudnn-windows-x86_64-8.4.1.50_cuda11.6-archive\bin\*
 Copy-Item -Path "d:\a\cudnn\cudnn-windows-x86_64-8.4.1.50_cuda11.6-archive\lib\*.*" -Destination "$($Env:ProgramFiles)\NVIDIA GPU Computing Toolkit\CUDA\v11.3\lib"
 Copy-Item -Path "d:\a\cudnn\cudnn-windows-x86_64-8.4.1.50_cuda11.6-archive\include\*.*" -Destination "$($Env:ProgramFiles)\NVIDIA GPU Computing Toolkit\CUDA\v11.3\include"
 
-Write-Output "cudnn copied"
-Write-Output "CUDA Installation completed "
+#Write-Output "CUDA Installation completed "
 
-$env:CUDA_PATH = "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.3"
-echo "CUDA_PATH=$env:CUDA_PATH" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
-echo "$env:CUDA_PATH/bin" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
+#$env:CUDA_PATH = "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.3"
+#echo "CUDA_PATH=$env:CUDA_PATH" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
+#echo "$env:CUDA_PATH/bin" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
